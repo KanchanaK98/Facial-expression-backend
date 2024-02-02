18c5a5936344a888c6819b2e7a9b47e8f3966a93
KanchanaK98 <kanchanakariyawasam98@gmail.com>
2024-02-02 08:51:09 +0530

    get image mimetype and changed controller logic

--- Controllers/mainController.js
+++ Controllers/mainController.js
@@ -1,6 +1,45 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const dotenv = require("dotenv");
+const fs = require("fs");
+const axios = require('axios');
+
+// Converts local file information to a GoogleGenerativeAI.Part object.
+function fileToGenerativePart(buffer, mimeType) {
+  return {
+    inlineData: {
+      data: buffer.toString("base64"),
+      mimeType
+    },
+  };
+}
+
+// Function to download an image from a URL and convert it to a Buffer
+const downloadImage = async (imageUrl) => {
+  try {
+    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+    return Buffer.from(response.data, 'binary');
+  } catch (error) {
+    console.error('Error downloading image:', error);
+    throw error;
+  }
+};
+const getImageMimeType = async (imageUrl) => {
+  try {
+    const response = await axios.head(imageUrl);
+    const contentType = response.headers['content-type'];
+    
+    if (!contentType) {
+      throw new Error('Content-Type header not found');
+    }
+
+    return contentType;
+  } catch (error) {
+    console.error('Error getting image MIME type:', error.message);
+    throw error;
+  }
+};
 exports.createResponse = async (req, res) => {
     console.log("start response...");
-    console.log(req.body.image);
     let file  = req.body.image;
     
   
@@ -10,43 +49,24 @@ exports.createResponse = async (req, res) => {
 
     let responseText = '';
     try {
-      const { GoogleGenerativeAI } = require("@google/generative-ai");
-      const dotenv = require("dotenv");
-      const fs = require("fs");
-      const axios = require('axios');
       
+
       dotenv.config();
       const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-      // Converts local file information to a GoogleGenerativeAI.Part object.
-      function fileToGenerativePart(buffer, mimeType) {
-        return {
-          inlineData: {
-            data: buffer.toString("base64"),
-            mimeType
-          },
-        };
-      }
-
-      // Function to download an image from a URL and convert it to a Buffer
-      const downloadImage = async (imageUrl) => {
-        try {
-          const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-          return Buffer.from(response.data, 'binary');
-        } catch (error) {
-          console.error('Error downloading image:', error);
-          throw error;
-        }
-      };
+      
   
       const imageBuffer = await downloadImage(file);
       // For text-and-image input (multimodal), use the gemini-pro-vision model
         const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
 
         const prompt = "This is a face of a human and what is the emotion this face express?Give me only a expression with only two words";
+        let mimeType
+        
+        mimeType = await getImageMimeType(file)
+        
 
-        const mimeType = 'image/jpeg';
-
+        console.log("mimeType : "+mimeType)
         const allowedMimeTypes = ['image/jpeg', 'image/png'];
         if (!allowedMimeTypes.includes(mimeType)) {
           return res.status(400).json({ message: 'Invalid file format. Only JPG and PNG are allowed.', success: false });
@@ -55,9 +75,6 @@ exports.createResponse = async (req, res) => {
           fileToGenerativePart(imageBuffer, mimeType),
         ];
 
-        
-      
-        
         try {
           const result = await model.generateContent([prompt, ...imageParts]);
           const response = await result.response;
@@ -75,7 +92,7 @@ exports.createResponse = async (req, res) => {
       return res.status(500).json({ message: "Internal server error", success: false });
     }
   
-    // You might want to return a response indicating that the generation process has started
+    
     return res.status(200).json({ message: responseText, success: true });
   };
   
